14d501b32a4f0887d4e61bb5898fdfe8dfb79e2b
chen4709 <chen4709@morris.umn.edu>
2017-02-04 16:53:59 -0600

    Allows to show a limited list, and todos with a specific status.

--- src/main/java/umm3601/todo/TodoController.java
+++ src/main/java/umm3601/todo/TodoController.java
@@ -2,6 +2,7 @@ package umm3601.todo;
 
 import com.google.gson.Gson;
 
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
@@ -27,12 +28,50 @@ public class TodoController {
             filteredTodos = limitTodosByNum(filteredTodos, limit);
         }
 
+        //filters by "status"
+        if(queryParams.containsKey("status")){
+            String status = queryParams.get("status")[0];
+            String True = "complete";
+            String False = "incomplete";
+            if(status.equals(True)){
+               filteredTodos = sortByStatus(filteredTodos, true);
+            }else if(status.equals(False)){
+               filteredTodos = sortByStatus(filteredTodos, false);
+            }
+        }
+
+        // filter by "contains"
+        if(queryParams.containsKey("contains")) {
+            String contains =queryParams.get("contains")[0];
+
+        }
+
         return filteredTodos;
     }
 
     // Limit Todos by number
     public Todo[] limitTodosByNum(Todo[] filteredTodos, int limit) {
         return Arrays.stream(filteredTodos, 0, limit).toArray(Todo[]::new);
+    }
+
+    // Sort by status
+    public Todo[] sortByStatus(Todo[] filteredTodos, boolean status){
+        return Arrays.stream(filteredTodos).filter(x -> x.status == status).toArray(Todo[]::new);
+    }
+
+    // Sort by contains
+//    public Todo[] sortByContains(Todo[] filteredTodos, String contains) {
+//        Todo[] todoCon;
+//
+//        return
+//    }
+
+    // find contains in one todo's body
+    public boolean checkBody(Todo todo, String contains) {
+        CharSequence cs = contains;
+        boolean bo;
+        bo = todo.body.contains(cs);
+        return bo;
     }
 
     // Get a single todo
